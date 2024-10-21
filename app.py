6c08081765fc0b111096bdc658b18583cb8277a3
David Depouez <ddepouez@gmail.com>
2024-10-21 14:25:04 +0200

    Add Database and exercices

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import duckdb
 import pandas as pd
 import streamlit as st
 
+con = duckdb.connect("Data/exercises_sql_tables.db", read_only=False)
+
 st.write(
     """
 SQL SRS
@@ -14,63 +16,49 @@ Space Repetition System SQL Practice
 
 with st.sidebar:
 
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review",
-        ("Joins", "GroupBy", "Windows Functions"),
+        ("Cross_joins", "GroupBy", "Windows Functions"),
         index=None,
         placeholder="Select a theme",
     )
+    st.write("You selected" ,theme)
 
-    st.write("You selected: ", option)
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}' ").df()
+    st.write(exercise)
 
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
+# ANSWER_STRING = """
+# SELECT * FROM beverages
+# CROSS JOIN food_items
+# """
+# solution_df = duckdb.sql(ANSWER_STRING).df()
 
-CSV2 = """
-food_item, food_price
-cookie juice,2.5
-bread with chocolate,2
-muffin, 3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
-
-ANSWER_STRING = """
-SELECT * FROM beverages
-CROSS JOIN food_items 
-"""
-
-solution_df = duckdb.sql(ANSWER_STRING).df()
 st.header("Enter your code:")
 sql_query = st.text_area(label="Enter an SQL request", key="user_input")
 if sql_query:
     result = duckdb.sql(sql_query).df()
     st.dataframe(result)
 
-    if solution_df.shape[0] != result.shape[0]:
-        st.write(
-            f"Result has {solution_df.shape[0] - result.shape[0]} differences with the solution_df"
-        )
-
-    try:
-        # set datframes columns in the same order
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-with tab2:
-    st.write("Table beverages")
-    st.dataframe(beverages)
-    st.write("Table food_items")
-    st.dataframe(food_items)
-    st.write("Expected")
-    st.dataframe(solution_df)
-
-with tab3:
-    st.write(ANSWER_STRING)
+#     if solution_df.shape[0] != result.shape[0]:
+#         st.write(
+#             f"Result has {solution_df.shape[0] - result.shape[0]} differences with the solution_df"
+#         )
+#
+#     try:
+#         # set datframes columns in the same order
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("Some columns are missing")
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+# with tab2:
+#     st.write("Table beverages")
+#     st.dataframe(beverages)
+#     st.write("Table food_items")
+#     st.dataframe(food_items)
+#     st.write("Expected")
+#     st.dataframe(solution_df)
+#
+# with tab3:
+#     st.write(ANSWER_STRING)
---
+++ init_db.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import duckdb
+import io
+
+con = duckdb.connect("Data/exercises_sql_tables.db", read_only=False)
+
+#---------------------------------------------------------------------------------
+# EXERCISES LIST
+#---------------------------------------------------------------------------------
+data = {
+    "theme" : ["Cross_joins","Windows_functions"],
+    "exercise_name": ["beverages_and_food","simple_window"],
+    "tables": [["beverages","food_items"],"simple_window"],
+    "Last_reviewed": ["1970-01-01","1970-01-01"]
+}
+memory_state_df = pd.DataFrame(data)
+con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+#---------------------------------------------------------------------------------
+# CROSS JOIN EXERCISES
+#---------------------------------------------------------------------------------
+CSV = """
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
+con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
+
+CSV2 = """
+food_item, food_price
+cookie juice,2.5
+bread with chocolate,2
+muffin, 3
+"""
+food_items = pd.read_csv(io.StringIO(CSV2))
+con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
